138256cdcab961d02013297e8e1962c8f03914c8
Mantas Santockis <mantas.san@gmail.com>
2023-03-23 23:15:35 +0200

    playing with children an siblings elements in h1

--- script.js
+++ script.js
@@ -386,3 +386,49 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
 //     this.style.backgroundColor = randomColor()
 //     console.log("nav", e.target);
 // })
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//TRAVERSING DOM
+
+//going downwords child
+
+const h1 = document.querySelector("h1")
+
+console.log(h1.querySelectorAll(".highlight"));
+
+console.log(h1.childNodes);
+
+console.log(h1.children);
+
+h1.firstElementChild.style.color = "white"
+h1.lastElementChild.style.color = "orangered"
+
+//going upwords: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+
+//recieve query string just like queryselector
+h1.closest(".header").style.background = "var(--gradient-secondary)"
+
+
+//queryselector finds childrens ant closest method finds parents of the element
+
+
+//going sideways: siblings
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+//making array and playing with siblings
+
+[...h1.parentElement.children].forEach(function (el) {
+    if (el !== h1) el.style.transform = "scale(0.5)"
+})
+
