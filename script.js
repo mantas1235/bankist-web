214c342f3f864ee552428636de475f18b28e9a65
Mantas Santockis <mantas.san@gmail.com>
2023-03-24 14:36:17 +0200

    Change navigation opacity when come over one of links

--- script.js
+++ script.js
@@ -135,10 +135,56 @@ tabsContainer.addEventListener("click", function (e) {
 
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Menu fade animations
 
 
+// const handleHover = function (e, opacity) {
+//     if (e.target.classList.contains("nav__link")) {
+//         const link = e.target
+//         const sibling = link.closest(".nav").querySelectorAll(".nav__link")
+//         const logo = link.closest(".nav").querySelector("img")
 
 
+//         sibling.forEach(el => {
+//             if (el !== link) el.style.opacity = opacity
+//         })
+//         logo.style.opacity = opacity
+//     }
+// }
+
+const nav = document.querySelector(".nav")
+
+//one of the way
+
+
+// nav.addEventListener("mouseover", function(e) {
+//     handleHover(e, 0,5)})
+
+// nav.addEventListener("mouseout", function(e) {
+//     handleHover(e, 0,5)})
+
+//other way with this keyword
+
+
+const handleHover = function (e) {
+    if (e.target.classList.contains("nav__link")) {
+        const link = e.target
+        const sibling = link.closest(".nav").querySelectorAll(".nav__link")
+        const logo = link.closest(".nav").querySelector("img")
+
+
+        sibling.forEach(el => {
+            if (el !== link) el.style.opacity = this
+        })
+        logo.style.opacity = this
+    }
+}
+
+nav.addEventListener("mouseover", handleHover.bind(0.5))
+
+nav.addEventListener("mouseout", handleHover.bind(1))
 
 
 
